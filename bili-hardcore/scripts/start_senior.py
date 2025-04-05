45cad7e18c5d98244f1ba25335945700320e376c
karben <karben233@gmail.com>
2025-04-05 14:52:38 +0800

    docs(start_senior): 添加分类ID输入提示信息

--- bili-hardcore/scripts/start_senior.py
+++ bili-hardcore/scripts/start_senior.py
@@ -81,7 +81,7 @@ class QuizSession:
             for cat in category.get('categories', []):
                 logger.info(f"ID: {cat.get('id')} - {cat.get('name')}")
             ids = input('请输入分类ID: ')
-
+            logger.info("tips: 输入多个分类ID请用 *英文逗号* 隔开,例如:1,2,3")
             logger.info("获取验证码...")
             captcha_res = captcha_get()
             logger.info("请打开链接查看验证码内容:{}".format(captcha_res.get('url')))
